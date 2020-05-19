6926f692af6d86f6faacfc2392fa4ebdf20a9103
Bhargav Chauhan <bhargavchauhan777@gmail.com>
2020-05-19 13:54:36 +0530

    Change seeder description and add classmap for app folder in autoload

--- database/seeds/PizzaSeeder.php
+++ database/seeds/PizzaSeeder.php
@@ -18,7 +18,7 @@ class PizzaSeeder extends Seeder
         $data = [
             [
                 'name' => 'Neapolitan Pizza',
-                'description' => 'The typical Neapolitan pizza toppings are fresh mozzarella, tomatoes, basil leaves, oregano, and olive oil. Since Neapolitan pizza is thin, it isn\'t designed to handle the weight of too many toppings. In fact, Neapolitan pizza is so thin that it\'s typically eaten with a fork and knife.',
+                'description' => 'The typical Neapolitan pizza toppings are fresh mozzarella, tomatoes, basil leaves, oregano, and olive oil. Since Neapolitan pizza is thin, it isn't designed to handle the weight of too many toppings. In fact, Neapolitan pizza is so thin that it's typically eaten with a fork and knife.',
                 'image' => 'images/paneer-makhani.jpg',
                 'sizes' => [1, 2, 3],
                 'crusts' => [1, 2, 3, 4],
@@ -39,7 +39,7 @@ class PizzaSeeder extends Seeder
             ],
             [
                 'name' => 'Chicago Pizza',
-                'description' => 'Chicago pizza, also commonly referred to as deep-dish pizza, gets its name from the city it was invented in. During the early 1900’s, Italian immigrants in the windy city were searching for something similar to the Neapolitan pizza that they knew and loved. Instead of imitating the notoriously thin pie, Ike Sewell had something else in mind. He created a pizza with a thick crust that had raised edges, similar to a pie, and ingredients in reverse, with slices of mozzarella lining the dough followed by meat, vegetables, and then topped with a can of crushed tomatoes. This original creation led Sewell to create the now famous chain restaurant, Pizzeria Uno.',
+                'description' => 'Chicago pizza, also commonly referred to as deep-dish pizza, gets its name from the city it was invented in. During the early 1900’s, Italian immigrants in the windy city were searching for something similar to the Neapolitan pizza that they knew and loved.',
                 'image' => 'images/paneer-makhani.jpg',
                 'sizes' => [1, 2, 3],
                 'crusts' => [1, 2, 3, 4],
@@ -60,7 +60,7 @@ class PizzaSeeder extends Seeder
             ],
             [
                 'name' => 'New York-Style Pizza',
-                'description' => 'New York-style pizza usually features tomato sauce and mozzarella cheese. Unlike its thin crust counterpart, the Neapolitan, New York-style pizzas can handle a wide range of additional toppings, from pepperoni and sausage to mushroom and anchovies. While this style of pizza can have virtually any topping added to it, it\'s common to find pizza lovers topping New York pizza with condiments, like oregano, red pepper flakes, Parmesan cheese, and garlic powder.',
+                'description' => 'New York-style pizza usually features tomato sauce and mozzarella cheese. Unlike its thin crust counterpart, the Neapolitan, New York-style pizzas can handle a wide range of additional toppings, from pepperoni and sausage to mushroom and anchovies.',
                 'image' => 'images/paneer-makhani.jpg',
                 'sizes' => [1, 2, 3],
                 'crusts' => [1, 2, 3, 4],
@@ -81,7 +81,7 @@ class PizzaSeeder extends Seeder
             ],
             [
                 'name' => 'Sicilian Pizza',
-                'description' => 'Sicilian pizza, also known as "sfincione," provides a thick cut of pizza with pillowy dough, a crunchy crust, and robust tomato sauce. This square-cut pizza is served with or without cheese, and often with the cheese underneath the sauce to prevent the pie from becoming soggy. Sicilian pizza was brought to America in the 19th century by Sicilian immigrants and became popular in the United States after the Second World War.',
+                'description' => 'Sicilian pizza, also known as "sfincione," provides a thick cut of pizza with pillowy dough, a crunchy crust, and robust tomato sauce. This square-cut pizza is served with or without cheese, and often with the cheese underneath the sauce to prevent the pie from becoming soggy.',
                 'image' => 'images/paneer-makhani.jpg',
                 'sizes' => [1, 2, 3],
                 'crusts' => [1, 2, 3, 4],
@@ -102,7 +102,7 @@ class PizzaSeeder extends Seeder
             ],
             [
                 'name' => 'Greek Pizza',
-                'description' => 'Greek pizza was created by Greek immigrants who came to America and were introduced to Italian pizza. Greek-style pizza, especially popular in the New England states, features a thick and chewy crust cooked in shallow, oiled pans, resulting in a nearly deep-fried bottom. While this style has a crust that is puffier and chewier than thin crust pizzas, it’s not quite as thick as a deep-dish or Sicilian crust.',
+                'description' => 'Greek pizza was created by Greek immigrants who came to America and were introduced to Italian pizza. Greek-style pizza, especially popular in the New England states, features a thick and chewy crust cooked in shallow, oiled pans, resulting in a nearly deep-fried bottom.',
                 'image' => 'images/paneer-makhani.jpg',
                 'sizes' => [1, 2, 3],
                 'crusts' => [1, 2, 3, 4],
@@ -123,7 +123,7 @@ class PizzaSeeder extends Seeder
             ],
             [
                 'name' => 'California Pizza',
-                'description' => 'California pizza, or gourmet pizza, is known for its unusual ingredients. This pizza got its start back in the late 1970’s when Chef Ed LaDou began experimenting with pizza recipes in the classic Italian restaurant, Prego. He created a pizza with mustard, ricotta, pate, and red pepper, and by chance, served it to Wolfgang Puck. Impressed with LaDou’s innovative pie, Puck invited him to be a head pizza chef at his restaurant. It was here that LaDou came up with over 250 unique pizza recipes that eventually formed the menu of the chain restaurant California Pizza Kitchen.',
+                'description' => 'California pizza, or gourmet pizza, is known for its unusual ingredients.This pizza got its start back in the late 1970’s when Chef Ed LaDou began experimenting with pizza recipes in the classic Italian restaurant, Prego. He created a pizza with mustard, ricotta, pate, and red pepper.',
                 'image' => 'images/paneer-makhani.jpg',
                 'sizes' => [1, 2, 3],
                 'crusts' => [1, 2, 3, 4],
@@ -144,7 +144,7 @@ class PizzaSeeder extends Seeder
             ],
             [
                 'name' => 'Detroit Pizza',
-                'description' => 'Reflecting the city’s deep ties to the auto industry, Detroit-style pizza was originally baked in a square automotive parts pan in the 1940’s. Detroit pizza is first topped with pepperoni, followed by brick cheese which is spread to the very edges of the pan, yielding a caramelized cheese perimeter. Sauce is then spooned over the pizza, an order similar to Chicago-style pizza. This pizza features a thick, extra crispy crust that is tender and airy on the inside.',
+                'description' => 'Reflecting the city’s deep ties to the auto industry, Detroit-style pizza was originally baked in a square automotive parts pan in the 1940’s. Detroit pizza is first topped with pepperoni, followed by brick cheese which is spread to the very edges of the pan.',
                 'image' => 'images/paneer-makhani.jpg',
                 'sizes' => [1, 2, 3],
                 'crusts' => [1, 2, 3, 4],
@@ -165,7 +165,7 @@ class PizzaSeeder extends Seeder
             ],
             [
                 'name' => 'St. Louis Pizza',
-                'description' => 'Looking for a light slice? St. Louis pizza features a thin crust with a cracker-like consistency that is made without yeast. Due to the crispy crust, St. Louis pizza is usually cut into three- or four-inch rectangles, known as "party" or "tavern" cut. This pizza features Provel processed cheese, which is a gooey combination of cheddar, Swiss, and provolone cheeses. St. Louis received an influx of Italian immigrants in the 19th century who were looking for employment opportunities. The Italian community, largely from Milan and Sicily, created the St. Louis-style pizza. Its sweet sauce is reminiscent of the Sicilian influence.',
+                'description' => 'Looking for a light slice? St. Louis pizza features a thin crust with a cracker-like consistency that is made without yeast. Due to the crispy crust, St. Louis pizza is usually cut into three- or four-inch rectangles, known as "party" or "tavern" cut.',
                 'image' => 'images/paneer-makhani.jpg',
                 'sizes' => [1, 2, 3],
                 'crusts' => [1, 2, 3, 4],
@@ -186,7 +186,7 @@ class PizzaSeeder extends Seeder
             ],
             [
                 'name' => 'Paneer Makhani',
-                'description' => 'Flavorful twist of spicy makhani sauce topped with paneer & capsicum',
+                'description' => 'Paneer Makhani Pizza is an Indian style pizza wherein we blend the best ever taste of Paneer Makhani which is also known as Paneer Butter Masala with every one\'s favorite Pizza',
                 'image' => 'images/paneer-makhani.jpg',
                 'sizes' => [1, 2, 3],
                 'crusts' => [1, 2, 3, 4],
@@ -211,6 +211,7 @@ class PizzaSeeder extends Seeder
             $pizza = new PizzaDetail();
             $pizza->name = $value['name'];
             $pizza->description = $value['description'];
+            $pizza->image = $value['image'];
             $pizza->save();
 
             foreach ($value['sizes'] as $size) {
