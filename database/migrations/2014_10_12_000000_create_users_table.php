e4b8f37e6a2ac8af2191a70e817a5d4786ac8b87
Bhargav Chauhan <bhargavchauhan777@gmail.com>
2020-05-19 12:11:29 +0530

    Change in User Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Utils\AppConstant;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -21,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('status')->default(AppConstant::STATUS_ACTIVE);
+            $table->boolean('status')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
